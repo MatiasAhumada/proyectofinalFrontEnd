4e4c47620f5d382bdb3051a9d2bb742913247459
thomas jerez <tjerez@outlook.com>
2022-11-15 19:56:41 -0300

    agregue el maquetado de la pagina admin para hacer la peticion GET

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,3 +1,17 @@
 
+const URL = 'http://localhost:3004/productos';
+
+// peticion GET
+
+
+export const consultarApi =async()=>{
+    try {
+        const respuesta = await fetch(URL);
+        const listaProductos = await respuesta.json()
+        return listaProductos;
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
