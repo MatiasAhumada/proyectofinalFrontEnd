0281c15ce6fc8d3a9512161d4833b3972fc8211a
matias <pagosptcmatias@gmail.com>
2022-11-16 10:16:18 -0300

    agregue peticiones put, delete y get id

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-
-import { Container } from 'react-bootstrap';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DetalleProducto from './components/views/DetalleProducto';
-import './App.css';
-import Inicio from './components/views/Inicio'
-import Navegar from './components/common/Navegar'
-import Pie from './components/common/Pie'
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetalleProducto from "./components/views/DetalleProducto";
+import "./App.css";
+import Inicio from "./components/views/Inicio";
+import Navegar from "./components/common/Navegar";
+import Pie from "./components/common/Pie";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Container>
-<Navegar></Navegar>
-   <BrowserRouter>
-   <Routes>
-    <Route
-    exact
-    path='/'
-    element={<Inicio></Inicio>}></Route>
-    <Route 
-     exact
-     path="/detalle-producto/:id"
-     element={<DetalleProducto></DetalleProducto>}
-    ></Route>
-   </Routes>
-   </BrowserRouter>
-<Pie></Pie>
-   </Container>
- 
+      <Navegar></Navegar>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Inicio></Inicio>}></Route>
+          <Route
+            exact
+            path="/detalle-producto/:id"
+            element={<DetalleProducto></DetalleProducto>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+      <Pie></Pie>
+    </Container>
   );
 }
 
---
+++ src/components/helpers/queris.js
@@ -0,0 +1,45 @@
+const URL = 'http://localhost:4001/apiEpik/productos'
+
+export const borrarProductoAPI = async (id) => {
+    try {
+    
+      const respuesta = await fetch(URL + "/" + id, {
+        method: "DELETE",
+      });
+  
+      return respuesta;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  export const obtenerProductoApi = async (id) => {
+    try {
+      
+      const respuesta = await fetch(URL + "/" + id);
+      const productoBuscado = {
+        dato: await respuesta.json(),
+        status: respuesta.status,
+      };
+  
+      // console.log(respuesta)
+      return productoBuscado;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  export const editarProductoApi = async (id, datosActualizados) => {
+    try {
+   
+      const respuesta = await fetch(URL + "/" + id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datosActualizados),
+      });
+      return respuesta;
+    } catch (error) {
+      console.log(error);
+    }
+  };
