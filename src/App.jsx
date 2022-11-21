349b62dc5183f4263521b692f28496b2fadc4167
matias <pagosptcmatias@gmail.com>
2022-11-21 16:19:24 -0300

    corregi el nombre de footer en app por conflicto con reactbootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,40 @@
-
-
-import { Container } from 'react-bootstrap';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DetalleProducto from './components/views/DetalleProducto';
-import './App.css';
-import Inicio from './components/views/Inicio'
-import Navegar from './components/common/Navegar'
-import Pie from './components/common/Pie'
-import Login from './components/views/Login';
-
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetalleProducto from "./components/views/DetalleProducto";
+import "./App.css";
+import Inicio from "./components/views/Inicio";
+import Nav from "./components/common/Nav";
+import Foot from "./components/common/Foot";
+import Login from "./components/views/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Register from "./components/views/Register";
-
-import { useState } from 'react';
-
-
+import { useState } from "react";
 
 function App() {
   const usuario = JSON.parse(localStorage.getItem("usuarioBar")) || [];
-  const [usuarioLogeado, setUsuarioLogeado] = useState(usuario)
+  const [usuarioLogeado, setUsuarioLogeado] = useState(usuario);
 
   return (
-
-
-    <Container>
-<Navegar></Navegar>
-   <BrowserRouter>
-   <Routes>
-    <Route
-    exact
-    path='/'
-    element={<Inicio></Inicio>}></Route>
-    <Route 
-     exact
-     path="/detalle-producto/:id"
-     element={<DetalleProducto></DetalleProducto>}
-    ></Route>
-      <Route exact path="/registro" element={<Register></Register>}/>
-    <Route exact path="/login" element={<Login setUsuarioLogeado={setUsuarioLogeado}></Login>} />
-   </Routes>
-   </BrowserRouter>
-<Pie></Pie>
-   </Container>
- 
-
-
+    <section>
+      <Foot></Foot>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Inicio></Inicio>}></Route>
+          <Route
+            exact
+            path="/detalle-producto/:id"
+            element={<DetalleProducto></DetalleProducto>}
+          ></Route>
+          <Route exact path="/registro" element={<Register></Register>} />
+          <Route
+            exact
+            path="/login"
+            element={<Login setUsuarioLogeado={setUsuarioLogeado}></Login>}
+          />
+        </Routes>
+      </BrowserRouter>
+      <Nav></Nav>
+    </section>
   );
 }
 
