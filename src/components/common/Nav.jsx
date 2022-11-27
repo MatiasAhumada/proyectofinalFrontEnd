5e71bc1e04986c755f03dc5cd300caa325598991
matias <pagosptcmatias@gmail.com>
2022-11-27 17:47:08 -0300

    pullenado errores

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,10 +1,10 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import "../../css/navbar.css";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link, NavLink } from "react-router-dom";
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+
 
 
 
