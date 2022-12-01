75a456262c0272cbce81855d5c8a5fd71d889865
matias <pagosptcmatias@gmail.com>
2022-11-30 21:11:31 -0300

    agregue cambios de tamaño de fuente y posisionamiento de colomna en el css de pagina detallesproductos

--- src/components/common/Foot.jsx
+++ src/components/common/Foot.jsx
@@ -1,32 +1,36 @@
-import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 import "../../css/footer.css";
 
 const Foot = () => {
-    return (
-      <footer >
-        <div className="fire text-center">
-          <div className="fire" id="fire1"></div>
-          <div className="fire" id="fire2"></div>
-          <div className="fire" id="fire3"></div>
-          <div className="fire" id="fire4"></div>
-        </div>
-        <ul className="social__icon">
-          <li><a href="https://github.com/MatiasAhumada/proyectofinalFrontEnd"><ion-icon name="logo-github"></ion-icon></a></li>
-          <li><a href={'/error404'}><ion-icon name="logo-instagram"></ion-icon>
-</a></li>
-          <NavLink to='/error404'><ion-icon name="logo-whatsapp"></ion-icon></NavLink>
-          
-        </ul>
-        <ul className="menu">
-        <NavLink to="/">Inicio</NavLink>
-<NavLink to="/nosotros">Nosotros</NavLink>  
-<NavLink to="/menu">Menu</NavLink>
-        </ul>
-        <p>&copy; Todos los derechos reservados</p>
-      
-      </footer>
-    );
-  };
-  
-  export default Foot;
-  
+  return (
+    <footer>
+      <div className="fire text-center">
+        <div className="fire" id="fire1"></div>
+        <div className="fire" id="fire2"></div>
+        <div className="fire" id="fire3"></div>
+        <div className="fire" id="fire4"></div>
+      </div>
+      <ul className="social__icon">
+        <Button href="https://github.com/MatiasAhumada/proyectofinalFrontEnd">
+          <ion-icon name="logo-github"></ion-icon>
+        </Button>
+        <Link to="/error404">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </Link>
+
+        <Link to="/error404">
+          <ion-icon name="logo-whatsapp"></ion-icon>
+        </Link>
+      </ul>
+      <ul className="menu">
+        <Link to="/">Inicio</Link>
+        <Link to="/nosotros">Nosotros</Link>
+        <Link to="/menu">Menu</Link>
+      </ul>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
+  );
+};
+
+export default Foot;
